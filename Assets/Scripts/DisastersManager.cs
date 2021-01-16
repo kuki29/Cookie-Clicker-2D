0cbd3c48d9ec9d3142fc94bd1fe36bcc431597f9
Vladislav <vladkukler@ukr.net>
2021-01-16 05:41:47 +0200

    First Commit. Already works basic features: - Make cookie - Sell cookie - Hire a baker to automaticly generate cookies - SFX and background music - Disasters, which can create some troubles for player - Display stats and messages

---
+++ Assets/Scripts/DisastersManager.cs
@@ -0,0 +1,251 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+using static GlobalResourceManagment;
+
+public class DisastersManager : MonoBehaviour
+{
+    enum DisasterType
+    {
+        Once,
+        Continuos
+    }
+
+    enum DisasterTarget
+    {
+        Cookies,
+        Money,
+        Bakers
+    }
+
+    enum DisasterDamageType
+    {
+        Percentage,
+        Amount
+    }
+
+    struct Disaster
+    {
+        public string Message;
+        public DisasterType Type;
+        public DisasterTarget Target;
+        public DisasterDamageType DamageType;
+        public int? MaxAmount;
+        public float? MaxPercentage;
+        public int? MaxDuration;
+    }
+
+
+    public GameObject statusDisplay;
+    [Range(0f, 1f)]
+    public float generatingChance = 0.1f;
+    [Range(0f, 1f)]
+    public float difficulty = 0.5f;
+
+    private bool isDisasterActive = false;
+    private List<Disaster> Disasters = new List<Disaster>
+        {
+            new Disaster
+            {
+                Message = "There is a fire in the kithen!",
+                Type = DisasterType.Continuos,
+                Target = DisasterTarget.Cookies,
+                DamageType = DisasterDamageType.Amount,
+                MaxAmount = 10,
+                MaxDuration = 15
+            },
+            new Disaster
+            {
+                Message = "Some of your workers decided to retire!",
+                Type = DisasterType.Once,
+                Target = DisasterTarget.Bakers,
+                DamageType = DisasterDamageType.Percentage,
+                MaxPercentage = 0.75f,
+            },
+            new Disaster
+            {
+                Message = "Someone robbed you!",
+                Type = DisasterType.Once,
+                Target = DisasterTarget.Money,
+                DamageType = DisasterDamageType.Percentage,
+                MaxPercentage = 0.99f
+            }
+        };
+
+    void Update()
+    {
+        if (!isDisasterActive)
+		{
+            StartCoroutine(DisastersLogic());
+		}
+    }
+
+    IEnumerator DisastersLogic()
+	{
+        isDisasterActive = true;
+        if (Random.Range(0f, 1f) < generatingChance)
+        {
+            int DisasterIndex = Random.Range(0, Disasters.Count);
+            StartCoroutine(DoDisaster(Disasters[DisasterIndex]));
+        }
+
+        yield return new WaitForSeconds(5);
+
+        statusDisplay.SetActive(false);
+        statusDisplay.SetActive(true);
+        isDisasterActive = false;
+    }
+
+    IEnumerator DoDisaster(Disaster disaster, int? duration = null)
+    {
+        string message = disaster.Message + "\nYou lost ";
+        int damage = 0;
+
+        /* Note: if we didn`t send duration of disaster, we check if it has max duration at all,
+         * in case it has we call DoDisaster() recursiwly and in inner calls we skip this statement,
+         * because we have duration now */
+
+        if (duration == null)
+		{
+            if (disaster.Type == DisasterType.Continuos)
+		    {
+                StartCoroutine(DoDisaster(disaster, Random.Range(Mathf.RoundToInt(disaster.MaxDuration.Value / 3f), disaster.MaxDuration.Value)));
+		    }
+		}
+
+        /* Note: if we have duration of a disaster we call DoDisaster() recursiwly for every 5 seconds
+         * of a duration of it */
+
+        if (duration.HasValue && duration.Value > 0)
+		{
+            StartCoroutine(DoDisaster(disaster, duration - 5));
+		}
+
+        if (disaster.DamageType == DisasterDamageType.Amount)
+        {
+            if (disaster.MaxAmount.HasValue)
+            {
+                damage = Random.Range(Mathf.RoundToInt(disaster.MaxAmount.Value / 3f), disaster.MaxAmount.Value);
+            }
+            else
+			{
+                Debug.LogError("Disaster wich has an amount type of damage must have a value of max amount of damage!!! ");
+                yield break;
+            }
+        }
+        else
+		{
+            if (disaster.MaxPercentage.HasValue)
+            {
+                damage = Mathf.RoundToInt(Random.Range(disaster.MaxPercentage.Value / 3f, disaster.MaxPercentage.Value));
+
+                if (disaster.Target == DisasterTarget.Bakers)
+				{
+                    damage *= bakersCount;
+				}
+                else if (disaster.Target == DisasterTarget.Cookies)
+				{
+                    damage *= cookieCount;
+				}
+                else if (disaster.Target == DisasterTarget.Money)
+				{
+                    damage *= cashCount;
+				}
+                else
+				{
+                    Debug.LogError("Unknown type of disaster target!!!");
+                    yield break;
+                }
+            }
+            else
+            {
+                Debug.LogError("Disaster wich has an amount type of damage must have a value of max amount of damage!!! ");
+                yield break;
+            }
+        }
+
+        damage = Mathf.RoundToInt(damage * difficulty);
+
+        if (damage == 0)
+		{
+            yield break;
+        }
+
+        switch (disaster.Target)
+		{
+            case DisasterTarget.Cookies:
+                if (cookieCount == 0)
+                {
+                    damage = 0;
+                    Debug.Log("Not enough cookies to process disaster");
+                    yield break;
+                }
+
+                if (damage > cookieCount)
+				{
+                    damage = cookieCount;
+                    cookieCount = 0;
+				}
+                else
+				{
+                    cookieCount -= damage;
+				}
+
+                message += damage + " cookies.";
+                break;
+
+            case DisasterTarget.Money:
+                if (cashCount == 0)
+                {
+                    damage = 0;
+                    Debug.Log("Not enough money to process disaster");
+                    yield break;
+                }
+
+                if (damage > cashCount)
+                {
+                    damage = cashCount;
+                    cashCount = 0;
+                }
+                else
+                {
+                    cashCount -= damage;
+                }
+
+                message += damage + "$.";
+                break;
+
+            case DisasterTarget.Bakers:
+                if (bakersCount == 0)
+                {
+                    damage = 0;
+                    Debug.Log("Not enough bakers to process disaster");
+                    yield break;
+                }
+
+                if (damage > bakersCount)
+                {
+                    damage = bakersCount;
+                    bakersCount = 0;
+                }
+                else
+                {
+                    bakersCount -= damage;
+                }
+
+                message += damage + " bakers.";
+                break;
+        }
+
+        if (damage > 0)
+		{
+            statusDisplay.GetComponent<Text>().text = message;
+            statusDisplay.GetComponent<Animation>().PlayQueued("Display1s");
+            statusDisplay.GetComponent<Animation>().PlayQueued("StatusAnim");
+		}
+
+        yield return new WaitForSeconds(5);
+    }
+}
---
+++ Assets/Scripts/GlobalResourceManagment.cs
@@ -0,0 +1,184 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class GlobalResourceManagment : MonoBehaviour
+{
+    public static int cookieCount = 0;
+    public static int cashCount = 0;
+    public static int bakersCount = 0;
+
+    public static int bakerPrice = 10;
+
+    public GameObject cookieDisplay;
+    public GameObject cashDisplay;
+    public GameObject bakersDisplay;
+    public GameObject bakersPriceDisplay;
+    public GameObject statusDisplay;
+
+    public GameObject bakersButton;
+
+    public GameObject sellCookieAudio;  // Note: Gameobject because it has two different sounds
+
+    public AudioSource buyBakerSound;
+    public AudioSource makeCookieSound;
+    public AudioSource buzzSound;
+
+    private bool isCreatingCookie = false;
+    private float productivity = 1f;
+
+    void Update()
+    {
+        DisplayStats();
+        DisplayBakersPrice();
+
+        CookieGenerator();
+
+        BuyButtonsAvailability();
+    }
+
+    public void DisplayStats()
+    {
+        DisplayCookiesAmount();
+        DisplayCashAmount();
+        DisplayBakersAmount();
+    }
+    
+    public void BuyBaker()
+    {
+        if (cashCount >= bakerPrice)
+        {
+            buyBakerSound.Play();
+            AddBaker();
+            if (bakersDisplay.active == false)
+			{
+                bakersDisplay.SetActive(true);
+			}
+        }
+        else
+        {
+            statusDisplay.GetComponent<Text>().text = "Not enough money to buy auto cookie.";
+            statusDisplay.GetComponent<Animation>().Play("StatusAnim");
+        }
+    }
+    
+    public void MakeCookie()
+	{
+        makeCookieSound.Play();
+        cookieCount++;
+    }
+
+    public void ProcessSellCookie()
+	{
+        if (cookieCount > 0)
+        {
+            ProcessSellClick();
+            PlaySellAudio();
+        }
+        else if (cookieCount == 0)
+        {
+            statusDisplay.GetComponent<Text>().text = "Not enough cookies to sell.";
+            statusDisplay.GetComponent<Animation>().Play("StatusAnim");
+            buzzSound.Play();
+        }
+    }
+
+
+    void DisplayCookiesAmount()
+    {
+        cookieDisplay.GetComponent<Text>().text = "Cookies: " + cookieCount;
+    }
+
+    void DisplayCashAmount()
+    {
+        cashDisplay.GetComponent<Text>().text = "$" + cashCount;
+    }
+
+    void DisplayBakersAmount()
+    {
+        bakersDisplay.GetComponent<Text>().text = "Bakers: " + bakersCount;
+    }
+
+    void DisplayBakersPrice()
+    {
+        bakersPriceDisplay.GetComponent<Text>().text = "$" + bakerPrice;
+    }
+
+    void BuyButtonsAvailability()
+    {
+        if (cashCount >= bakerPrice)
+        {
+            bakersButton.GetComponent<Button>().interactable = true;
+        }
+    }
+
+    void SellCookies(int amount)
+    {
+        if (cookieCount > amount)
+        {
+            cookieCount -= amount;
+            cashCount += amount;
+        }
+        else
+        {
+            cashCount += cookieCount;
+            cookieCount = 0;
+        }
+    }
+
+    // TODO: rewrite it for multi purpose
+    void ProcessSellClick()
+	{
+        if (Input.GetKey(KeyCode.LeftControl))
+        {
+            SellCookies(25);
+        }
+        else if (Input.GetKey(KeyCode.LeftShift))
+        {
+            SellCookies(10);
+        }
+        else if (Input.GetKey(KeyCode.LeftAlt))
+        {
+            SellCookies(5);
+        }
+        else
+        {
+            SellCookies(1);
+        }
+    }
+
+    void PlaySellAudio()
+	{
+        sellCookieAudio.GetComponents<AudioSource>()[Random.Range(0, 2)].Play();
+    }
+
+    void CookieGenerator()
+    {
+        if (bakersCount > 0)
+        {
+            if (!isCreatingCookie)
+            {
+                isCreatingCookie = true;
+                StartCoroutine(AutoCreateCookie());
+            }
+        }
+    }
+
+    IEnumerator AutoCreateCookie()
+    {
+        cookieCount += Mathf.RoundToInt(bakersCount * productivity);
+        yield return new WaitForSeconds(1);
+        isCreatingCookie = false;
+    }
+
+    void AddBaker()
+    {
+        if (cashCount >= bakerPrice)
+		{
+            cashCount -= bakerPrice;
+            bakersCount++;
+            bakerPrice = bakersCount * 7 + 10;
+		}
+    }
+}
