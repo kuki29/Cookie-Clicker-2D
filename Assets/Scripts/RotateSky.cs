b795342742d0a41505cbff411d5b54a15037a6a1
Vladislav <vladkukler@ukr.net>
2021-01-16 16:39:31 +0200

    Added background skybox rotation

---
+++ Assets/Scripts/RotateSky.cs
@@ -0,0 +1,16 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class RotateSky : MonoBehaviour
+{
+    [Range(0f, 1f)]
+    public float rotationSpeed = .1f;
+
+    private float rotation = 0f;
+
+    void Update()
+    {
+        RenderSettings.skybox.SetFloat("_Rotation", rotation += rotationSpeed);
+    }
+}
