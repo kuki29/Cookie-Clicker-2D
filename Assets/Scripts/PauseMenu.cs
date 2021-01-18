6b5a6df64702f9c3da6fd1be0ee44795759a4003
Vladislav <vladkukler@ukr.net>
2021-01-18 02:17:04 +0200

    Save and Load game refactored

--- Assets/Scripts/PauseMenu.cs
+++ Assets/Scripts/PauseMenu.cs
@@ -26,31 +26,11 @@ public class PauseMenu : MonoBehaviour
 
 	public void SaveGame()
 	{
-		PlayerPrefs.SetInt("cookies", GlobalResources.cookiesCount);
-		PlayerPrefs.SetInt("money", GlobalResources.moneyCount);
-		PlayerPrefs.SetInt("bakers", GlobalEmployees.bakersCount);
-		PlayerPrefs.SetInt("sellManagers", GlobalEmployees.sellManagersCount);
-		PlayerPrefs.SetInt("madeCookies", Statistics.madeCookies);
-		PlayerPrefs.SetInt("madeMoney", Statistics.madeMoney);
-		PlayerPrefs.SetInt("spendMoney", Statistics.spendMoney);
-		PlayerPrefs.SetInt("hiredBakers", Statistics.hiredBakers);
-		PlayerPrefs.SetInt("hiredSellManagers", Statistics.hiredSellManagers);
-		PlayerPrefs.SetFloat("disastersGeneratingChance", mechanicsObject.GetComponent<DisastersManager>().generatingChance);
-		PlayerPrefs.SetFloat("disastersDifficulty", mechanicsObject.GetComponent<DisastersManager>().difficulty);
+		Saves.Save(mechanicsObject.GetComponent<DisastersManager>());
 	}
 
 	public void LoadGame()
 	{
-		GlobalResources.cookiesCount = PlayerPrefs.GetInt("cookies");
-		GlobalResources.moneyCount = PlayerPrefs.GetInt("money");
-		GlobalEmployees.bakersCount = PlayerPrefs.GetInt("bakers");
-		GlobalEmployees.sellManagersCount = PlayerPrefs.GetInt("sellManagers");
-		Statistics.madeCookies = PlayerPrefs.GetInt("madeCookies");
-		Statistics.madeMoney = PlayerPrefs.GetInt("madeMoney");
-		Statistics.spendMoney = PlayerPrefs.GetInt("spendMoney");
-		Statistics.hiredBakers = PlayerPrefs.GetInt("hiredBakers");
-		Statistics.hiredSellManagers = PlayerPrefs.GetInt("hiredSellManagers");
-		mechanicsObject.GetComponent<DisastersManager>().generatingChance = PlayerPrefs.GetFloat("disastersGeneratingChance");
-		mechanicsObject.GetComponent<DisastersManager>().difficulty = PlayerPrefs.GetFloat("disastersDifficulty");
+		Saves.Load(mechanicsObject.GetComponent<DisastersManager>());
 	}
 }
---
+++ Assets/Scripts/Saves.cs
@@ -0,0 +1,36 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Saves : MonoBehaviour
+{
+	public static void Save(DisastersManager disastersManager)
+	{
+		PlayerPrefs.SetInt("cookies", GlobalResources.cookiesCount);
+		PlayerPrefs.SetInt("money", GlobalResources.moneyCount);
+		PlayerPrefs.SetInt("bakers", GlobalEmployees.bakersCount);
+		PlayerPrefs.SetInt("sellManagers", GlobalEmployees.sellManagersCount);
+		PlayerPrefs.SetInt("madeCookies", Statistics.madeCookies);
+		PlayerPrefs.SetInt("madeMoney", Statistics.madeMoney);
+		PlayerPrefs.SetInt("spendMoney", Statistics.spendMoney);
+		PlayerPrefs.SetInt("hiredBakers", Statistics.hiredBakers);
+		PlayerPrefs.SetInt("hiredSellManagers", Statistics.hiredSellManagers);
+		PlayerPrefs.SetFloat("disastersGeneratingChance", disastersManager.generatingChance);
+		PlayerPrefs.SetFloat("disastersDifficulty", disastersManager.difficulty);
+	}
+
+	public static void Load(DisastersManager disastersManager)
+	{
+		GlobalResources.cookiesCount = PlayerPrefs.GetInt("cookies");
+		GlobalResources.moneyCount = PlayerPrefs.GetInt("money");
+		GlobalEmployees.bakersCount = PlayerPrefs.GetInt("bakers");
+		GlobalEmployees.sellManagersCount = PlayerPrefs.GetInt("sellManagers");
+		Statistics.madeCookies = PlayerPrefs.GetInt("madeCookies");
+		Statistics.madeMoney = PlayerPrefs.GetInt("madeMoney");
+		Statistics.spendMoney = PlayerPrefs.GetInt("spendMoney");
+		Statistics.hiredBakers = PlayerPrefs.GetInt("hiredBakers");
+		Statistics.hiredSellManagers = PlayerPrefs.GetInt("hiredSellManagers");
+		disastersManager.generatingChance = PlayerPrefs.GetFloat("disastersGeneratingChance");
+		disastersManager.difficulty = PlayerPrefs.GetFloat("disastersDifficulty");
+	}
+}
